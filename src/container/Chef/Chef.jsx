b17656d21e5db65dfbb5336881f4a327e241c246
Abu Bakar <absoud4@gmail.com>
2022-12-05 20:42:34 +0600

    website done

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+
+import './Chef.css';
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What We Believe In</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            necessitatibus quia dolore nostrum tenetur maxime?
+          </p>
+        </div>
+        <p className="p__opensans">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe facere
+          iure magni praesentium vitae aut laboriosam unde iste officiis
+          temporibus!
+        </p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="" />
+      </div>
+
+    </div>
+  </div>
+);
+
+export default Chef;
